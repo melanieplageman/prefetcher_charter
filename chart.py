160788e90c1542848448c1ea94e79c8c60cc2cee
Melanie Plageman <melanieplageman@gmail.com>
2022-12-14 13:12:04 -0500

    set x and y limits

--- chart.py
+++ chart.py
@@ -30,7 +30,9 @@ def get_wait_df(wait_log, xmin, xmax):
     data = []
     for key, val in output.items():
         data.append({'tick': key, 'wait': val})
-    return pd.DataFrame.from_records(data, index='tick')
+    df = pd.DataFrame.from_records(data)
+    df.set_index('tick', drop=False)
+    return df
 
 width = 15
 height = 20
@@ -39,35 +41,59 @@ figure = plt.figure(figsize=(width, height))
 class ChartGroupCollection:
     def __init__(self, chart_groups):
         self.members = chart_groups
-        self._xmin = None
-        self._xmax = None
 
-    @property
-    def xmin(self):
-        if self._xmin is not None:
-            return self._xmin
-        self._xmin = self.members[0].xmin
-        for chart_group in self.members:
-            if chart_group.xmin < self._xmin:
-                self._xmin = chart_group.xmin
-        return self._xmin
-
-    @property
-    def xmax(self):
-        if self._xmax is not None:
-            return self._xmax
-        self._xmax = self.members[0].xmax
+    def global_absolute_xmax(self):
+        global_xmax = self.members[0].xmax
         for chart_group in self.members:
             if chart_group.xmax > self._xmax:
-                self._xmax = chart_group.xmax
-        return self._xmax
+                global_xmax = chart_group.xmax
+        return global_xmax
 
-    def plot(self, axes):
+    def plot(self):
+        # rows is the number of different chart types -- currently is completions,
+        # consumptions, and waits
+        max_nrows = 3
+        ncols = len(self.members)
+        width = 11 * ncols
+        height = 5 * max_nrows
+
+        figure = plt.figure(figsize=(width, height))
+        axes = figure.subplots(max_nrows, ncols, squeeze=False)
+
+        for member in self.members:
+            member.make_time_relative()
+
+        global_relative_xmax = 0
+        global_nios_ymax = 0
+        global_latency_ymax = 0
+        for member in self.members:
+            member_relative_xmax = member.get_max_relative_time()
+            if member_relative_xmax > global_relative_xmax:
+                global_relative_xmax = member_relative_xmax
+
+            member_nios_ymax = member.get_nios_max()
+            if member_nios_ymax > global_nios_ymax:
+                global_nios_ymax = member_nios_ymax
+
+            member_latency_ymax = member.completions.latency.max()
+            if member_latency_ymax > global_latency_ymax:
+                global_latency_ymax = member_latency_ymax
+
+        global_nios_ymax *= 1.2
+        global_latency_ymax *= 1.2
+        global_relative_xmax *= 1.1
+
+        for col, member in enumerate(self.members):
+            axes[member.nios_row][col].set_ylim([0, global_nios_ymax])
+            axes[member.latency_row][col].set_ylim([0, global_latency_ymax])
+
         for ax in axes.flat:
-            ax.set_xlim([self.xmin, self.xmax])
+            ax.set_xlim([0, global_relative_xmax])
 
-        for chart_group in self.members:
-            chart_group.plot(axes)
+        for col, chart_group in enumerate(self.members):
+            chart_group.plot(col, axes)
+
+        plt.savefig('current.png')
 
 
 class ChartGroup:
@@ -79,20 +105,23 @@ class ChartGroup:
         self._consumptions = None
         self._xmax = None
         self._xmin = None
+        self.wait_row = 0
+        self.nios_row = 1
+        self.latency_row = 2
 
     @property
     def xmin(self):
         if self._xmin is not None:
             return self._xmin
-        self._xmin = math.floor(min(self.completions.submission_time))
+        self._xmin = math.floor(self.completions.submission_time.min())
         return self._xmin
 
     @property
     def xmax(self):
         if self._xmax is not None:
             return self._xmax
-        completion_xmax = math.ceil(max(self.completions.completion_time))
-        consumption_xmax = math.ceil(max(self.consumptions.consumption_time))
+        completion_xmax = math.ceil(self.completions.completion_time.max())
+        consumption_xmax = math.ceil(self.consumptions.consumption_time.max())
         self._xmax = max(completion_xmax, consumption_xmax)
         return self._xmax
 
@@ -122,20 +151,49 @@ class ChartGroup:
         self._consumptions = pd.read_csv(consumption_log)
         return self._consumptions
 
-    def plot(self, axes):
-        self.waits.plot.area(stacked=False, ax=axes[0][0])
-        self.completions.plot(y=['inflight', 'cnc', 'prefetch_distance'], ax=axes[1][0])
-        self.completions.plot(y=['latency'], ax=axes[2][0])
-        output_image_filename = self.version + '.png'
-        plt.savefig(output_image_filename)
+    def make_time_relative(self):
+        completions_zero = self.completions.completion_time.min()
+        self._completions['relative_time'] = self.completions.completion_time.apply(
+            lambda t: t - completions_zero)
+
+        consumptions_zero = self.consumptions.consumption_time.min()
+        self._consumptions['relative_time'] = self.consumptions.consumption_time.apply(
+            lambda t: t - consumptions_zero)
+
+        waits_zero = self.waits.tick.min()
+        self._waits['relative_time'] = self.waits.tick.apply(
+            lambda t: t - waits_zero)
+
+    def get_max_relative_time(self):
+        return max(
+                self.completions.relative_time.max(),
+                self.waits.relative_time.max(),
+                self.consumptions.relative_time.max()
+                )
+
+    def get_nios_max(self):
+        return max(
+                self.completions.inflight.max(),
+                self.completions.cnc.max(),
+                self.completions.prefetch_distance.max()
+                )
+
+    def plot(self, col, axes):
+        self.waits.plot.area(x='relative_time', stacked=False,
+                             ax=axes[self.wait_row][col])
+
+        self.completions.plot(x='relative_time', y=['inflight', 'cnc',
+                                                    'prefetch_distance'],
+                              ax=axes[self.nios_row][col])
+
+        self.completions.plot(x='relative_time', y=['latency'],
+                              ax=axes[self.latency_row][col])
+
+        # Put the title above the first row of each column
+        axes[0][col].set_title(self.version)
 
 
-versions = ['patched']
-# rows is the number of different chart types -- currently is completions,
-# consumptions, and waits
-max_nrows = 3
-ncols = len(versions)
-axes = figure.subplots(max_nrows, ncols, squeeze=False)
+versions = [ 'patched','og']
 
 chart_groups = []
 for version in versions:
@@ -143,4 +201,4 @@ for version in versions:
 
 cg_collection = ChartGroupCollection(chart_groups)
 
-cg_collection.plot(axes)
+cg_collection.plot()
